d8932c37d5d418f0065e5189ff5d88fc2424d3c7
shubhamsahu03 <71839040+shubhamsahu03@users.noreply.github.com>
2024-03-21 11:44:46 +0530

    Update NewsAdapter.java

--- app/src/main/java/com/example/ecoera12/NewsAdapter.java
+++ app/src/main/java/com/example/ecoera12/NewsAdapter.java
@@ -18,7 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-// Based on ud843-Quake Report - Udemy Android Basics Application
+
 
 /**
  * An {@link NewsAdapter} knows how to create a list item layout for each news item
